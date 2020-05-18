4c869596c221327844f214a25b6aa321e2fc6164
Kuldeep Kapade <kuldeepkapade@gmail.com>
2020-05-18 16:14:48 -0700

    Revert "Revert "Updated allowed hosts""
    This reverts commit ab809e28a657d91305af3c972ace96d55f8b9857.

--- project/settings.py
+++ project/settings.py
@@ -66,7 +66,7 @@ SECRET_KEY = get_secret('django', {}).get('secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
